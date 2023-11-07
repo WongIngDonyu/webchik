a124ae4b7a6c0b6f85775e4f0b76830663034fce
Ёлка <wongingdonyu@gmail.com>
2023-11-07 17:05:41 +0300

    minor commit

--- src/main/java/com/example/webchick/init/DataInitializer.java
+++ src/main/java/com/example/webchick/init/DataInitializer.java
@@ -38,33 +38,33 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     private void seedData() throws IOException {
-        BrandDto b1 = new BrandDto(UUID.randomUUID(),"Zoo");
+        BrandDto b1 = new BrandDto(null,"Zoo");
         BrandDto sb1 = brandService.add(b1);
-        BrandDto b2 = new BrandDto(UUID.randomUUID(),"ZOV");
+        BrandDto b2 = new BrandDto(null,"ZOV");
         BrandDto sb2 = brandService.add(b2);
         sb1.setName("LOX");
         brandService.update(sb1);
-        ModelDto m1 = new ModelDto(UUID.randomUUID(),"Zoo1", Model.Category.Motorcycle, "SomeUrl", 2023, 2054,"Zoo");
+        ModelDto m1 = new ModelDto(null,"Zoo1", Model.Category.Motorcycle, "SomeUrl", 2023, 2054,"Zoo");
         ModelDto modelDto = modelService.add(m1);
         modelDto.setName("alallala");
         modelDto.setBrand("ZOV");
         modelService.update(modelDto);
-        ModelDto m2 = new ModelDto(UUID.randomUUID(),"Zoo2222", Model.Category.Motorcycle, "SomeUrl", 2023, 2054,"LOX");
+        ModelDto m2 = new ModelDto(null,"Zoo2222", Model.Category.Motorcycle, "SomeUrl", 2023, 2054,"LOX");
         ModelDto modelDto1 = modelService.add(m2);
         //System.out.println(modelDto.getBrand());
-        UserRoleDto userRoleDto = new UserRoleDto(UUID.randomUUID(), UserRole.Role.User);
+        UserRoleDto userRoleDto = new UserRoleDto(null, UserRole.Role.User);
         UserRoleDto userRoleDto1 = userRoleService.add(userRoleDto);
         userRoleDto1.setRole(UserRole.Role.Admin);
         userRoleService.update(userRoleDto1);
-        UserDto userDto = new UserDto(UUID.randomUUID(), "1","1488","Egor", "Ananasiy", true, "someUrl", UserRole.Role.Admin);
+        UserDto userDto = new UserDto(null, "1","1488","Egor", "Ananasiy", true, "someUrl", UserRole.Role.Admin);
         UserDto userDro1 = userService.add(userDto);
         userDro1.setActive(false);
         userService.update(userDro1);
-        UserRoleDto userRoleDto2 = new UserRoleDto(UUID.randomUUID(), UserRole.Role.User);
+        UserRoleDto userRoleDto2 = new UserRoleDto(null, UserRole.Role.User);
         UserRoleDto userRoleDto3 = userRoleService.add(userRoleDto2);
         userDro1.setRole(UserRole.Role.User);
         userService.update(userDro1);
-        OfferDto offerDto = new OfferDto(UUID.randomUUID(),"somekool", Offer.Engine.DIESEL, "someUrl", 100, 100000, Offer.Transmission.AUTOMATIC, 2003,"1", "alallala");
+        OfferDto offerDto = new OfferDto(null,"somekool", Offer.Engine.DIESEL, "someUrl", 100, 100000, Offer.Transmission.AUTOMATIC, 2003,"1", "alallala");
         OfferDto offerDto1 = offerService.add(offerDto);
         System.out.println(userService.findByUsername(offerDto1.getUsername()));
         offerDto1.setPrice(6666);
